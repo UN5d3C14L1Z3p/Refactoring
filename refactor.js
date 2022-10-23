ff284296f80261d5a49637506ec977f5a6b7ba38
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-23 12:59:59 +0800

    Refactor using Change Function Declaration (URL: https://refactoring.com/catalog/changeFunctionDeclaration.html).

--- refactor.js
+++ refactor.js
@@ -24,7 +24,7 @@ function statement(invoice, plays) {
 
 function amountFor(aPerformance, play) {
 	let result = 0;
-	switch (play.type) {
+	switch (playFor(aPerformance).type) {
 		case "tragedy":
 			result = 40000;
 			if (aPerformance.audience > 30) {
@@ -38,7 +38,7 @@ function amountFor(aPerformance, play) {
 			}
 			break;
 		default:
-			throw new Error(`unknown type: ${play.type}`);
+			throw new Error(`unknown type: ${playFor(aPerformance).type}`);
 	}
 	return result;
 }
