c405cfd55b79684a1cab9234dbc057bde5aa8f83
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-29 12:35:38 +0800

    (Bad commit: Recent local changes where also 'pushed') Refactor using Extraction Function (URL: https://refactoring.com/catalog/extractFunction.html). [Pushed] Refactor using Replace Temp with Query (URL: https://refactoring.com/catalog/replaceTempWithQuery.html).

--- refactor.js
+++ refactor.js
@@ -7,10 +7,7 @@ function statement(invoice, plays) {
 		result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
 		totalAmount += amountFor(perf);
 	}
-	let VolumeCredits = 0;
-	for (let perf of invoice.performances) {
-		volumeCredits += volumeCreditsFor(perf);
-	}
+	let VolumeCredits = totalVolumeCredits();
 
 	result += `Amount owed is ${usd(totalAmount/100)}\n`;
 	result += `You earned ${volumeCredits} credits\n`;
@@ -53,4 +50,12 @@ function usd(aNumber) {
 	return new Intl.NumberFormat("en-US",
 		{ style: "currency", currency: "USD",
 			minimumFractionDigits: 2 }).format(aNumber);
+}
+
+function totalVolumeCredits() {
+	let volumeCredits = 0;
+	for (let perf of invoice.performances) {
+		volumeCredits += VolumeCreditsFor(perf);
+	}
+	return volumeCredits;
 }
