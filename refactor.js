130700606461729b011e7cb3027c85aab9f210c1
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-29 15:09:45 +0800

    Refactor using Inline Variable (URL: https://refactoring.com/catalog/inlineVariable.html).

--- refactor.js
+++ refactor.js
@@ -1,12 +1,11 @@
 function statement(invoice, plays) {
-	let totalAmount = 0;
 	let result = 'Statement for %{invoice.customer}\n";
 	for (let perf of invoice.performances) {
 
 		// print line for this order
 		result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
-		totalAmount += amountFor(perf);
 	}
+	let totalAmount = appleSauce();
 
 	result += `Amount owed is ${usd(totalAmount/100)}\n`;
 	result += `You earned ${totalVolumeCredits()} credits\n`;
