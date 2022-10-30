51348469e993b613f51ed90b025ec3fb9e09cb86
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-30 11:26:56 +0800

    Delete parameter `invoice` to `renderPlainText`.

--- refactor.js
+++ refactor.js
@@ -1,17 +1,17 @@
 function statement(invoice, plays) {
 	const statementData = {};
 	statementData.customer = invoice.customer;
-	return renderPlainText(statementData, invoice, plays);
+	statementData.performances = invoice.performances;
+	return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
 	let result = 'Statement for %{data.customer}\n";
-	for (let perf of invoice.performances) {
+	for (let perf of data.performances) {
 
 		// print line for this order
 		result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
 	}
-	let totalAmount = appleSauce();
 
 	result += `Amount owed is ${usd(totalAmount())}\n`;
 	result += `You earned ${totalVolumeCredits()} credits\n`;
